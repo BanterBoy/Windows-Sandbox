37bca0892c050a6856013078d857a66b4af60857
Luke Leigh <luke.leigh@gmail.com>
2021-11-28 19:16:30 +0000

    Update sandbox-config.ps1

--- WSBshare/sandbox-config.ps1
+++ WSBshare/sandbox-config.ps1
@@ -10,7 +10,8 @@ Start-Job {Install-Module PSReleaseTools -Force; Install-PowerShell -Mode Quiet 
 Start-Job {Install-Module WTToolbox -Force; Install-WTRelease}
 Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1
 Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1
-Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\sandbox-toast.ps1
 
 #wait for everything to finish
 Get-Job | Wait-Job
+
+Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Sandbox-Toast.ps1
