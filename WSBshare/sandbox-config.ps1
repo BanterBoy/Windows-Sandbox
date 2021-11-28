fb25dea8f84be7f0f957f24b259d14da4e13de33
Luke Leigh <luke.leigh@gmail.com>
2021-11-28 22:20:48 +0000

    Update sandbox-config.ps1

--- WSBshare/sandbox-config.ps1
+++ WSBshare/sandbox-config.ps1
@@ -4,9 +4,8 @@ Install-PackageProvider -Name nuget -Force -ForceBootstrap -Scope AllUsers
 Update-Module PackageManagement,PowerShellGet -Force
 
 #run updates and installs in the background
-Start-Job {Install-Module PSScriptTools,PSTeachingTools -Force}
+Start-Job {Install-Module PSScriptTools,PSTeachingTools,BurntToast -Force}
 Start-Job {Install-Module PSReleaseTools -Force; Install-PowerShell -Mode Quiet -EnableRemoting -EnableContextMenu}
-
 Start-Job {Install-Module WTToolbox -Force; Install-WTRelease}
 Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1
 Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1
