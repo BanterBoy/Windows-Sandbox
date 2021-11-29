3539f2de17b8f7735df2b381b3ab1d08186af65c
Luke Leigh <luke.leigh@gmail.com>
2021-11-29 02:09:35 +0000

    Update sandbox-setup.cmd

--- WSBshare/sandbox-setup.cmd
+++ WSBshare/sandbox-setup.cmd
@@ -4,7 +4,9 @@ REM Create my standard Work folder
 mkdir c:\OutPutDir
 
 REM set execution policy first so that a setup script can be run
-powershell.exe -command "&{ Set-ExecutionPolicy -ExecutionPolicy Unrestricted }"
+powershell.exe -command "&{ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser }"
 
 REM Now run the true configuration script
 powershell.exe -file C:\GitRepos\Windows-Sandbox\WSBshare\sandbox-config.ps1
+
+
