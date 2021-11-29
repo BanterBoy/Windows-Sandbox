287aad68b90e0bad326ae5114d14f22c7f42d6c3
Luke Leigh <luke.leigh@gmail.com>
2021-11-29 02:11:05 +0000

    Update sandbox-config.ps1

--- WSBshare/sandbox-config.ps1
+++ WSBshare/sandbox-config.ps1
@@ -2,10 +2,12 @@ Enable-PSRemoting -Force -SkipNetworkProfileCheck
 Install-PackageProvider -Name nuget -Force -ForceBootstrap
 Update-Module PackageManagement,PowerShellGet -Force
 
+#run updates and installs in the background
 Start-Job { Install-Module PSReleaseTools -Force; Install-PowerShell -Mode Quiet -EnableRemoting -EnableContextMenu -EnableRunContext }
 Start-Job { Install-Module WTToolbox -Force; Install-WTRelease }
 Start-Job { Install-Module BurntToast -Force }
 Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1
+Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1
 
 # Wait for everything to finish
 Get-Job | Wait-Job
@@ -16,16 +18,4 @@ $params = @{
     Header = $(New-BTHeader -Id 1 -Title "Sandbox Complete")
     Applogo = "C:\GitRepos\Windows-Sandbox\WSBshare\ToastIcon.jpg"
 }
-
 New-BurntToastNotification @params
-
-
-#run updates and installs in the background
-# Start-Job {Install-Module PSScriptTools,PSTeachingTools,BurntToast -Force}
-# Start-Job {Install-Module PSReleaseTools -Force; Install-PowerShell -Mode Quiet -EnableRemoting -EnableContextMenu}
-# Start-Job {Install-Module WTToolbox -Force; Install-WTRelease}
-# Start-Job {Install-Module BurntToast -Force}
-# Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1
-# Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1
-# Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Sandbox-Toast.ps1
-
