b7a73a549451340b5128cb61cf4adf9f204bf92e
Rob Green <robert.green@yourkeys.com>
2021-11-30 20:35:38 +0000

    Build config file dynamically and allow path to be set

--- WSBshare/sandbox-config.ps1
+++ WSBshare/sandbox-config.ps1
@@ -1,13 +1,18 @@
+param([string]$repoPath)
+
+mkdir c:\OutPutDir
+
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
 Install-PackageProvider -Name nuget -Force -ForceBootstrap
-Update-Module PackageManagement,PowerShellGet -Force
+Install-Module PackageManagement,PowerShellGet -Force
+# Update-Module PackageManagement,PowerShellGet -Force
 
 #run updates and installs in the background
 Start-Job { Install-Module PSReleaseTools -Force; Install-PowerShell -Mode Quiet -EnableRemoting -EnableContextMenu -EnableRunContext }
 Start-Job { Install-Module WTToolbox -Force; Install-WTRelease }
 Start-Job { Install-Module BurntToast -Force }
-Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1
-Start-Job -FilePath C:\GitRepos\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1
+Start-Job -FilePath "$repoPath\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1" -ArgumentList $repoPath
+Start-Job -FilePath "$repoPath\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1" -ArgumentList $repoPath
 
 # Wait for everything to finish
 Get-Job | Wait-Job
@@ -16,6 +21,6 @@ Get-Job | Wait-Job
 $params = @{
     Text = "Windows Sandbox configuration is complete."
     Header = $(New-BTHeader -Id 1 -Title "Sandbox Complete")
-    Applogo = "C:\GitRepos\Windows-Sandbox\WSBshare\ToastIcon.jpg"
+    Applogo = "$repoPath\Windows-Sandbox\WSBshare\ToastIcon.jpg"
 }
 New-BurntToastNotification @params
