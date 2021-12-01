d85c58a97fc0e1725585c2bbb1041ee2d99d8118
Luke Leigh <luke.leigh@gmail.com>
2021-12-01 02:33:44 +0000

    Update sandbox-config.ps1

--- WSBshare/sandbox-config.ps1
+++ WSBshare/sandbox-config.ps1
@@ -4,7 +4,7 @@ mkdir c:\OutPutDir
 
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
 Install-PackageProvider -Name nuget -Force -ForceBootstrap
-Install-Module PackageManagement,PowerShellGet -Force
+Install-Module PackageManagement, PowerShellGet -Force
 # Update-Module PackageManagement,PowerShellGet -Force
 
 #run updates and installs in the background
@@ -14,13 +14,26 @@ Start-Job { Install-Module BurntToast -Force }
 Start-Job -FilePath "$repoPath\Windows-Sandbox\WSBshare\Set-SandboxDesktop.ps1" -ArgumentList $repoPath
 Start-Job -FilePath "$repoPath\Windows-Sandbox\WSBshare\Install-VSCodeSandbox.ps1" -ArgumentList $repoPath
 
+function Copy-PSProfile {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $ProfilePath = "C:\GitRepos\ProfileFunctions\Microsoft.PowerShell_profile.ps1"
+    )
+    if (Test-Path $ProfilePath) {
+        Copy-Item -Path $ProfilePath -Destination $PROFILE
+    }
+}
+Copy-PSProfile
+
 # Wait for everything to finish
 Get-Job | Wait-Job
 
 # Sandbox Configuration Complete Toast Notification
 $params = @{
-    Text = "Windows Sandbox configuration is complete."
-    Header = $(New-BTHeader -Id 1 -Title "Sandbox Complete")
+    Text    = "Windows Sandbox configuration is complete."
+    Header  = $(New-BTHeader -Id 1 -Title "Sandbox Complete")
     Applogo = "$repoPath\Windows-Sandbox\WSBshare\ToastIcon.jpg"
 }
 New-BurntToastNotification @params
